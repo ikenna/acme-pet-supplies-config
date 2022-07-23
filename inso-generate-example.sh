a0cedc63dba41d5b27d46ba10ff20c205397a633
Ikenna Nwaiwu <ikenna4u@gmail.com>
2022-07-23 07:32:14 +0100

    DEV-acc-test-with-gen: Finish acceptance test with generation

---
+++ inso-generate-example.sh
@@ -0,0 +1,1 @@
+inso --verbose generate config --type declarative --tags deck-managed --output inso-generated-kong-config.yaml ../acme-pet-supplies/reference/compliant-product-catalog-api.oas.yaml 
---
+++ postgres-status.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pg_ctl status -D /usr/local/var/postgres
