cf7e6cfcc74c1d30652ed555bf083bdddae566d0
Ikenna Nwaiwu <ikenna4u@gmail.com>
2022-07-25 07:35:26 +0100

    Renaming workflow

--- inso-generate-example.sh
+++ inso-generate-example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-inso --verbose generate config --type declarative --tags deck-managed --output inso-generated-kong-config.yaml ../acme-pet-supplies/reference/compliant-product-catalog-api.oas.yaml 
+inso --verbose generate config --type declarative --tags deck-managed --output inso-generated-kong-config.yaml ../acme-pet-supplies/api-definitions/product-catalog-api.oas.yaml 
